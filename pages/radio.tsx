46c19526bbeb6c9db1ae6f7641d64dcf3f63b8ed
Carti <hexanlv.2@gmail.com>
2024-02-09 15:52:23 -0500

    Added more songs

--- pages/radio.tsx
+++ pages/radio.tsx
@@ -2,23 +2,50 @@ import React, { useState } from 'react';
 
 const VideoPlayer: React.FC = () => {
   const [playRequested, setPlayRequested] = useState(false);
+  const [audioIndex, setAudioIndex] = useState(0);
 
   const handlePlayRequest = () => {
     setPlayRequested(true);
   };
 
+  const handleDoubleClick = () => {
+    setAudioIndex(prevIndex => (prevIndex + 1) % audioFiles.length);
+  };
+
+  // Array of audio files
+  const audioFiles = ['uptownfunk.mp3', 'partyrock.mp3', 'heyya.mp3', 'shboom.mp3', 'afterparty.mp3', 'funkytown.mp3', 'wakemeup.mp3']; // Add more audio files as needed
+
+  // Function to get the current audio file
+  const getCurrentAudioFile = () => {
+    return audioFiles[audioIndex];
+  };
+
   return (
-    <div style={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#000', position: 'relative' }}>
+    <div 
+      style={{ 
+        width: '100%', 
+        height: '100vh', 
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        backgroundColor: '#000', 
+        position: 'relative' 
+      }}
+      onDoubleClick={handleDoubleClick}
+    >
       {!playRequested && (
         <div style={{ position: 'absolute', zIndex: 1, cursor: 'pointer' }} onClick={handlePlayRequest}>
           <h1 style={{ color: '#fff', fontSize: '24px' }}>Click to Play</h1>
         </div>
       )}
       {playRequested && (
-        <video style={{ width: '100%', height: 'auto' }} autoPlay loop>
-          <source src="/raccoon.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+        <React.Fragment>
+          <video style={{ width: '100%', height: 'auto' }} autoPlay loop muted>
+            <source src="/raccoon.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <audio src={`/audio/${getCurrentAudioFile()}`} autoPlay loop />
+        </React.Fragment>
       )}
     </div>
   );
