7a059baa3266f83602bc657ba56fa065a7adcf2a
venqoi (nolan) <hexanlv.2@gmail.com>
2024-07-07 19:44:01 -0400

    Added privacy and tos

---
+++ pages/tos.tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  background: ;
+  color: #afb6af;
+  padding: 10px;
+`;
+
+const TestText = styled.div`
+  font-size: 1rem;
+  font-weight: 800;
+`;
+
+const ToS: NextPage = () => {
+  return (
+    <Wrapper>
+      <Head>
+        <title>racc.lol - tos</title>
+      </Head>
+      <TestText>very big tos blah blah <br></br>pretty much don't spam it and no ddossing. Use it however you'd like! </TestText>
+    </Wrapper>
+  );
+};
+
+export default ToS;
