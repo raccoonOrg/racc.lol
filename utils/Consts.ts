f06c5983b80b66128cfdc393b336828af730e00d
nolan <nolan@racc.lol>
2026-05-06 11:40:14 -0400

    Fixed repo link and removed unused images

--- utils/Consts.ts
+++ utils/Consts.ts
@@ -6,8 +6,8 @@ export type Testimonial = {
 };
 
 export const api_url = "https://api.racc.lol";
-export const frontend_repo = "https://github.com/venqoi/raccoon-web";
-export const api_repo = "https://github.com/venqoi/racc-api";
+export const frontend_repo = "https://github.com/raccoonOrg/racc.lol";
+export const api_repo = "https://github.com/raccoonOrg/api.racc.lol";
 export const me = "https://venqoi.lol";
 export const radio = "https://racc.lol/radio";
 
@@ -16,7 +16,7 @@ export const TESTIMONIALS: Testimonial[] = [
   {
     message: `racc.lol is super great and so easy to use I love it it's perfect for all my projects for maximum trash panda cuteness`,
     name: "Keircn",
-    pfp: "/img/Keircn.png",
+    pfp: "/img/keircn.png",
     worksAt: "Priory IO",
   },
   {
